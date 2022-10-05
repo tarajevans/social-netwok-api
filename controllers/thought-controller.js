b6d9825d383dea7cb66748c65d271647405fe19c
Tara Evans <tarajevans@hotmail.com>
2022-10-05 18:46:19 -0400

    set up routes and schemas for project

---
+++ controllers/thought-controller.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+// // example data
+// {
+//   "thoughtText": "Here's a cool thought...",
+//   "username": "lernantino",
+//   "userId": "5edff358a0fcb779aa7b118b"
+// }
